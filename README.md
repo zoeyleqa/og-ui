89fd1f29121b56968da0033ce6cc2f0342f7da66
Brandon Shea <bsshea5@gmail.com>
2021-09-16 11:18:15 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Notes
+
+Followed this tutorial to get started: https://prateeksurana.me/blog/react-component-library-using-storybook-6/
+
+## Run Storybook
+
+> yarn storybook
+
+## Build
+
+> yarn build
---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import postcss from "rollup-plugin-postcss";
+
+const packageJson = require("./package.json");
+
+export default {
+  // entry point for Rollup for our component library
+  input: "src/index.ts",
+  // output key indicates what types of output files will be generated at which place
+  output: [
+    {
+      file: packageJson.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    postcss({
+      extensions: [".css"],
+    }),
+  ],
+};
